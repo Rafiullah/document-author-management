789ccee0dd59694b2d1f2ca5d7347d9a97341986
rafiullah <rafiullahmommand@yahoo.com>
2024-09-12 17:14:47 +0200

    Initial Setup

---
+++ src/main/java/com/momand/docauthor/DocumentAuthorManagementApplication.java
@@ -0,0 +1,13 @@
+package com.momand.docauthor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentAuthorManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentAuthorManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/momand/docauthor/DocumentAuthorManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momand.docauthor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentAuthorManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
