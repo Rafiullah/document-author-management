c01e483d0e2fd63fd7ba4059aa35c61b356c1507
rafiullah <rafiullahmommand@yahoo.com>
2024-09-13 12:22:26 +0200

    Refactored Entities -- removed Lombok

--- src/main/java/com/momand/docauthor/document/entity/DocumentEntity.java
+++ src/main/java/com/momand/docauthor/document/entity/DocumentEntity.java
@@ -3,42 +3,76 @@ package com.momand.docauthor.document.entity;
 import com.momand.docauthor.author.entity.AuthorEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "document_entity")
 public class DocumentEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     @NotNull(message = "Title is required for a Document.")
     private String title;
     @NotNull(message = "Body is required for a Document.")
     private String body;
 
-    //@ManyToMany(cascade = CascadeType.PERSIST)
-    //@ManyToMany(cascade = CascadeType.MERGE)
-    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST})
     @JoinTable(
             name = "article_author",
             joinColumns = @JoinColumn(name = "article_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id")
     )
+    //@JsonIgnoreProperties("documents")
+    //@JsonManagedReference
     private Set<AuthorEntity> authors = new HashSet<>();
 
-    //@ManyToMany
-    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST})
     @JoinTable(name = "document_references",
             joinColumns = @JoinColumn(name = "document_id"),
             inverseJoinColumns = @JoinColumn(name = "reference_document_id"))
     private Set<DocumentEntity> references = new HashSet<>();
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Set<AuthorEntity> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<AuthorEntity> authors) {
+        this.authors = authors;
+    }
+
+    public Set<DocumentEntity> getReferences() {
+        return references;
+    }
+
+    public void setReferences(Set<DocumentEntity> references) {
+        this.references = references;
+    }
 }
