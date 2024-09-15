14e367d386014c74187a108d882d161ea0e87210
rafiullah <rafiullahmommand@yahoo.com>
2024-09-15 18:03:15 +0200

    GlobalExceptionHandler added

---
+++ src/main/java/com/momand/docauthor/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.momand.docauthor.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> handleNotFoundException(NotFoundException ex, WebRequest request){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
